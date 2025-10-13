0e137a311307e9451a79f17e74169d551939bb15
Mohamed shaban <166997119+mohamed-710@users.noreply.github.com>
2025-10-14 02:59:54 +0300

    Update User.model.js

--- section 1/Topic1.2/User.model.js
+++ section 1/Topic1.2/User.model.js
@@ -1,8 +1,8 @@
+const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },          // must be provided
   email: { type: String, required: true, unique: true },  // must be unique
   age: { type: Number, min: 18 },                   // must be at least 18
   createdAt: { type: Date, default: Date.now }      // auto-filled
 });
-
 const User = mongoose.model('User', userSchema);
